be3492ea74f67916424641ee5c14cf7d4bf7619c
Stefano Simoni <stefano.simoni.10@hotmail.com.ar>
2024-06-28 10:26:11 -0300

    Commit 1
    Users CRUD terminado.

--- backend/db/daos/usersDaoMySQL.js
+++ backend/db/daos/usersDaoMySQL.js
@@ -1,17 +1,33 @@
 import { connection } from "../connections/mySQLConnection.js";
 
-const createUser = () => {};
-
-const readUsers = async () => {
-  const query = "SELECT * FROM Users";
+const createUser = async (user) => {
+  const query = `INSERT INTO Users (name, last_name, email, password, birthday, country, isAdmin) VALUES ("${user.name}", "${user.last_name}", "${user.email}", "${user.password}", "${user.birthday}", "${user.country}", ${user.isAdmin})`;
   const [result] = await connection.promise().query(query);
   return result;
 };
 
-const readUser = (id) => {};
+const readUsers = async () => {
+  const query = `SELECT * FROM Users`;
+  const [result] = await connection.promise().query(query);
+  return result;
+};
 
-const updateUser = (id) => {};
+const readUser = async (id) => {
+  const query = `SELECT * FROM Users WHERE id = ${id}`;
+  const [result] = await connection.promise().query(query);
+  return result;
+};
 
-const deleteUser = (id) => {};
+const updateUser = async (user) => {
+  const query = `UPDATE Users SET name = "${user.name}", last_name = "${user.last_name}", email = "${user.email}", password = "${user.password}", birthday = "${user.birthday}", country = "${user.country}", isAdmin = ${user.isAdmin} WHERE id = ${user.id}`;
+  const [result] = await connection.promise().query(query);
+  return result;
+};
+
+const deleteUser = async (id) => {
+  const query = `DELETE FROM Users WHERE id = ${id}`;
+  const [result] = await connection.promise().query(query);
+  return result;
+};
 
 export { createUser, readUsers, readUser, updateUser, deleteUser };
